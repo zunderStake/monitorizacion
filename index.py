6da42c44b10af3acb88f9135f537b688f3c32881
moisesrodriguez <moises.rodriguez@vitro.bio>
2021-02-18 21:54:44 +0100

    20210218

--- index.py
+++ index.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
+import os, sys
+import subprocess
+
 app = Flask(__name__)
 #mysql conexion
 app.config['MYSQL_HOST'] = 'localhost'
@@ -17,6 +20,46 @@ def Index():
     cur_mysql.execute('SELECT * FROM contacts')
     data = cur_mysql.fetchall()
     return render_template('add_contact.html', contacts=data)
+
+@app.route('/autodiscovery')
+def AutoDiscovery():
+    return render_template('autodiscovery.html')
+
+@app.route('/search_autodiscovery',methods=['POST'])
+def Search_AutoDiscovery():
+    if request.method=='POST':
+        def ping(ip_ping):
+            p = subprocess.Popen(['ping', '-n', '1', '-w', '2', ip_ping])
+            p.wait()
+            return '1' if p.poll() == 0 else '0'
+        ip_initial = request.form['ip_initial']
+        ip_finished = request.form['ip_finished']
+        first_ip_octates = ip_initial.split(".")
+        finished_ip_octates = ip_finished.split(".")
+        ip_incrementa = int(first_ip_octates[3])
+        ip_final_incrementa = int(finished_ip_octates[3])
+        numero_server = 0
+        if first_ip_octates[0]==finished_ip_octates[0]:
+            if first_ip_octates[1] == finished_ip_octates[1]:
+                if first_ip_octates[2] == finished_ip_octates[2]:
+                    while ip_incrementa <= ip_final_incrementa:
+                        ip_incrementa_string = str(ip_incrementa)
+                        ip_ping =first_ip_octates[0] + "." + first_ip_octates[1] + "." + first_ip_octates[2] + "." + ip_incrementa_string
+                        response = int(ping(ip_ping))
+                        if response == 1:
+                            cur_mysql = mysql.connection.cursor()
+                            cur_mysql.execute('INSERT INTO inventory_ip (IP, NOMBRE, ACTIVE) VALUES (%s, %s, %s)',(ip_ping, ip_ping, 1))
+                            mysql.connection.commit()
+                            numero_server += 1
+                        else:
+                            pingstatus = "Network Error"
+                        ip_incrementa += 1
+        flash('Autodiscovery terminado, se han agregado %s IP.',(numero_server))
+        return render_template('inventory.html')
+
+@app.route('/inventory')
+def Inventory():
+    return render_template('inventory.html')
 
 @app.route('/add_contact',methods=['POST'])
 def add_contact():
